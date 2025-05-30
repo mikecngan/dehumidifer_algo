be0dac4a00151c6f2dd3347de80be3cfd3397ae2
mikecngan <mikecngan@gmail.com>
2025-05-30 17:24:38 -0500

    made a wide range

--- sense_store_control.py
+++ sense_store_control.py
@@ -31,7 +31,7 @@ async def main():
 			avg_humidity = statistics.mean(humidity_history)
 		
 		#run algo to decide if dehumidifier should be on
-		if bme280_data.humidity > target_humidity + 2:
+		if bme280_data.humidity > target_humidity + 3:
 			up_counter = up_counter + 1
 
 		if bme280_data.humidity < target_humidity - 2:
